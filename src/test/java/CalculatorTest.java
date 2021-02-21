1e159f9a91e0c9e303a5384e8d7d47ba7410ac17
Riptangulo <Riptangulo@Desktop-dmn>
2021-02-21 22:19:18 +0100

    Primera contribución.

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,37 @@
+
+
+import com.fernandez_martinez_daniel.calculator.Calculator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() { // Create object before compilation
+        calculator = new Calculator();
+    }
+
+    /*
+     * testCalc() test method
+     */
+    @Test
+    void testCalc() {
+        double first = 3;
+        String second = "5";
+
+        try {
+            Assertions.assertEquals(8, calculator.calc(first, second, '+', null));
+            Assertions.assertEquals(-2, calculator.calc(first, second, '-', null));
+            Assertions.assertEquals(15, calculator.calc(first, second, '*', null));
+            Assertions.assertEquals(0.6, calculator.calc(first, second, '/', null));
+            Assertions.assertEquals(3d, calculator.calc(first, second, '%', null));
+            Assertions.assertEquals(243, calculator.calc(first, second, '^', null));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
